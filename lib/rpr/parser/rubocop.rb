d8426ff97f4fe221c31a88c4a4af97736ad29ce9
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-12-27 15:58:32 +0900

    Support Ruby 2.6

--- lib/rpr/parser/rubocop.rb
+++ lib/rpr/parser/rubocop.rb
@@ -4,7 +4,10 @@ module Rpr
   module Parser
     module Rubocop
       def self.parse(code)
-        r = RuboCop::ProcessedSource.new(code, RUBY_VERSION.to_f)
+        version = RUBY_VERSION.to_f
+        # XXX: Parser gem does not support Ruby 2.6
+        version = 2.5 if version == 2.6
+        r = RuboCop::ProcessedSource.new(code, version)
         return r.ast
       end
     end
