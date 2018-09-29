4e0045e2d3b846077376e6d36a9418722b80b362
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-09-29 12:07:30 +0900

    Support JSON formater for Parser::AST::Node

--- lib/rpr/formatter/json.rb
+++ lib/rpr/formatter/json.rb
@@ -4,6 +4,7 @@ module Rpr
   module Formatter
     module Json
       def self.print(object)
+        object = object.to_sexp_array if defined?(::Parser::AST::Node) && object.is_a?(::Parser::AST::Node)
         puts JSON.pretty_generate(object)
       end
     end
